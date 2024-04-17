78cd0ed2a124f2c6a16ac4ef881b2d84fd14f7eb
Alenxandre silva <xvalexandrexv@gmail.com>
2024-04-17 04:27:25 +0100

    text modification

--- components/about.tsx
+++ components/about.tsx
@@ -10,7 +10,7 @@ export default function About() {
   return (
     <motion.section 
     ref={ref}
-    className="mb- 28 max-w-[45rem] text-center leading-8
+    className="mb-28 max-w-[45rem] text-center leading-8
     sm:mb-40 scroll-mt-28"
     initial={{ opacity: 0, y: 100 }}
     animate={{ opacity: 1, y: 0 }}
@@ -39,11 +39,11 @@ export default function About() {
     </p>
 
     <p>
-    <span className="italic">When I'm not coding</span>, I enjoy playing
+    <span className="italic">When Im not coding</span>, I enjoy playing
         video games, watching movies, and playing with my dog. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
+        <span className="font-medium">history and philosophy</span>. Im also
         learning how to play the guitar.
     </p>
 

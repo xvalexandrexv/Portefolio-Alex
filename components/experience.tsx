dc488e555cfbe3e56b9e1b33921d16524d37b970
Alenxandre silva <xvalexandrexv@gmail.com>
2024-04-17 04:41:09 +0100

    inview Modification

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,10 +1,13 @@
-"use client"; 
+"use client";
+
 import React from "react";
 import SectionHeading from "./section-heading";
-import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "@/lib/data";
-import { useInView } from "react-intersection-observer";
 import { useSectionInView } from "@/lib/hooks";
 import { useTheme } from "@/context/theme-context";
 
@@ -12,44 +15,42 @@ export default function Experience() {
   const { ref } = useSectionInView("Experience");
   const { theme } = useTheme();
 
-  // Initialize an array to store refs and inView states for each experience item
-  const experiencesInView = experiencesData.map(() => {
-    // Call useInView hook to track visibility for each experience item
-    const { ref, inView } = useInView({ threshold: 0 });
-    return { ref, inView };
-  });
-
   return (
-    <section ref={ref} id="experience" className="scroll-mt-28 mb-28 sm:40">
+    <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My experience</SectionHeading>
       <VerticalTimeline lineColor="">
-        {/* Iterate over experiencesData and render VerticalTimelineElement */}
         {experiencesData.map((item, index) => (
-          <div key={index} ref={experiencesInView[index].ref} className="vertical-timeline-element">
+          <React.Fragment key={index}>
             <VerticalTimelineElement
               contentStyle={{
-                background: theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
+                background:
+                  theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
                 boxShadow: "none",
                 border: "1px solid rgba(0, 0, 0, 0.05)",
                 textAlign: "left",
                 padding: "1.3rem 2rem",
               }}
               contentArrowStyle={{
-                borderRight: theme === "light" ? "0.4rem solid #9ca3af" : "0.4rem solid rgba(255, 255, 255, 0.15)",
+                borderRight:
+                  theme === "light"
+                    ? "0.4rem solid #9ca3af"
+                    : "0.4rem solid rgba(255, 255, 255, 0.5)",
               }}
-              visible={experiencesInView[index].inView}
               date={item.date}
               icon={item.icon}
               iconStyle={{
-                background: theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
+                background:
+                  theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
                 fontSize: "1.5rem",
               }}
             >
               <h3 className="font-semibold capitalize">{item.title}</h3>
-              <p className="!mt-0 font-normal">{item.location}</p>
-              <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">{item.description}</p>
+              <p className="font-normal !mt-0">{item.location}</p>
+              <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
+                {item.description}
+              </p>
             </VerticalTimelineElement>
-          </div>
+          </React.Fragment>
         ))}
       </VerticalTimeline>
     </section>
